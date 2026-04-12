e044a67402f54e5381cb142dc1f0017a7a86772a
Byanajjara Ganesha <byanajjaraganesha821@gmail.com>
2026-04-12 13:05:53 +0530

    solve: step_one - output always high

---
+++ 01_basics/step_one.v
@@ -0,0 +1,47 @@
+// Code your design here
+
+
+
+
+
+
+
+
+Day-22
+
+Clock-synchronized driver
+
+Day-23
+
+Driver → Monitor handshake stability
+
+Day-24
+
+Functional coverage improvements
+
+Day-25
+
+Assertions for ALU operations
+
+Day-26
+
+Test configuration system
+
+Day-27
+
+Multiple test scenarios
+
+Day-28
+
+Error injection testing
+
+Day-29
+
+Project cleanup + documentation
+
+Day-30
+
+Final industry-style GitHub project
+
+
+

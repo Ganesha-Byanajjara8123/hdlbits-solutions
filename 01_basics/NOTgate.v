1e0893c3dfb946ca6548ab87719dfe7abb7415fa
Byanajjara Ganesha <byanajjaraganesha821@gmail.com>
2026-04-14 19:21:24 +0530

    solve: NOTgate-Basics

---
+++ 01_basics/NOTgate.v
@@ -0,0 +1,12 @@
+// ============================================
+// HDLBits Problem : NOT gate
+// Topic          : Basics
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-14
+// Link           : https://hdlbits.01xz.net/wiki/Notgate
+// ============================================
+
+
+module top_module( input in, output out );
+assign out = ~in;
+endmodule

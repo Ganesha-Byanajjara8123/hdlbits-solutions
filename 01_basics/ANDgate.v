916bd9f6ded239d015350f89b5a1e52cdd15ba7b
Byanajjara Ganesha <byanajjaraganesha821@gmail.com>
2026-04-15 21:00:20 +0530

    solve: ANDgate-AND gate operation

---
+++ 01_basics/ANDgate.v
@@ -0,0 +1,17 @@
+// ============================================
+// HDLBits Problem : NOT gate
+// Topic          : Basics
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-15
+// Link           : https://hdlbits.01xz.net/wiki/Andgate
+// ============================================
+
+
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+    
+    assign out = a & b;
+
+endmodule

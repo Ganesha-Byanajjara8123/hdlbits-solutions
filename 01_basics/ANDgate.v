9b7c192025cc1931836b4b32002717a73562db37
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-15 21:30:15 +0530

    Update ANDgate.v

--- 01_basics/ANDgate.v
+++ 01_basics/ANDgate.v
@@ -1,5 +1,5 @@
 // ============================================
-// HDLBits Problem : NOT gate
+// HDLBits Problem : AND gate
 // Topic          : Basics
 // Solved by      : Byanajjara Ganesha
 // Date           : 2026-04-15

4830f906fcf795a1cc564e60e24559f2eaf9ba54
Byanajjara Ganesha <byanajjaraganesha821@gmail.com>
2026-04-16 19:49:11 +0530

    solve: wire_decl-more wires

---
+++ 01_basics/IC_7458.v
@@ -0,0 +1,28 @@
+// ============================================
+// HDLBits Problem : NOT gate
+// Topic          : Basics
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-15
+// Link           : https://hdlbits.01xz.net/wiki/7458
+// ============================================
+
+
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    
+    wire p2_ab, p1_abc, p2_cd, p1_def;
+    
+    assign p2_ab = p2a & p2b;
+    assign p1_abc = p1a & p1b & p1c;
+    assign p2_cd = p2c & p2d;
+    assign p1_def = p1d & p1e & p1f;
+    
+    assign p1y = p1_abc | p1_def;
+    assign p2y = p2_ab  | p2_cd;
+    
+
+
+endmodule
---
+++ 01_basics/wire_decl.v
@@ -0,0 +1,27 @@
+
+// ============================================
+// HDLBits Problem : NOT gate
+// Topic          : Basics
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-15
+// Link           : https://hdlbits.01xz.net/wiki/Wire_decl
+// ============================================
+
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    //declare wire from 2-AND gates and as input for OR gate
+    wire ab_in, cd_in;
+    
+    //assigning/connecting the wire to respective gates
+    assign ab_in = a&b;
+    assign cd_in = c&d;
+    assign out   = ab_in | cd_in;
+    assign out_n = ~out;
+    
+endmodule

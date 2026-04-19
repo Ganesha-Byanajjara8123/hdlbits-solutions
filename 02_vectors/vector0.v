0c02bf3a2f4e7aef7723771e058078f32710b62f
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-19 09:45:11 +0530

    Add files via upload
    solve: vector0 - 3-bit vector wire

---
+++ 02_vectors/vector0.v
@@ -0,0 +1,29 @@
+// ============================================
+<<<<<<< HEAD
+// HDLBits Problem : Vector0
+// Topic          : Basics
+=======
+// HDLBits Problem :Vecotr0
+// Topic          : Vectors
+>>>>>>> 187b57103655556c156433b8fb222bc7ecc45426
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-18
+// Link           : https://hdlbits.01xz.net/wiki/Vector0
+// ============================================
+
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); // Module body starts after module declaration.
+    
+    assign outv = vec;
+    assign o2   = vec[2];
+    assign o1   = vec[1];
+    assign o0   = vec[0];
+    
+    //or we can done it like in the cancatination form
+    //assign {o2, o1, o0} = vec ;
+
+endmodule

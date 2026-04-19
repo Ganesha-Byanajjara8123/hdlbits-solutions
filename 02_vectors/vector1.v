38398ab6717e00c2ad9ab22eedac7b9e1b1ab02f
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-19 09:48:19 +0530

    solve: vector1 - vector part select

---
+++ 02_vectors/vector1.v
@@ -0,0 +1,20 @@
+// ============================================
+// HDLBits Problem : Vector1
+// Topic          : Vectors
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-15
+// Link           : https://hdlbits.01xz.net/wiki/Vector1
+// ============================================
+
+
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    
+    assign out_hi = in [15:8];  //upper bits
+    assign out_lo = in [7:0];   //lower bits
+ 
+    
+endmodule

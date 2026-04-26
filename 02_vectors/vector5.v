7fa74e4365b9071221ca7aa8d4fb3a294de641ee
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-26 08:54:32 +0530

    solve: vector5-concatenating and replicating the five inputs
    Implement XNOR operation on concatenated input vectors.

---
+++ 02_vectors/vector5.v
@@ -0,0 +1,20 @@
+// ============================================
+// HDLBits Problem : Vector
+// Topic          : Vector5
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-24
+// Link           : https://hdlbits.01xz.net/wiki/Vector5
+// ============================================
+
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    // assign out = ~{ ... } ^ { ... };
+    
+    //based on the previous problem: solution
+     assign out = ~{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {{5{a,b,c,d,e}}};
+
+endmodule

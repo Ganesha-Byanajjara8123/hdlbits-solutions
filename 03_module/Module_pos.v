931504473802c4ee02ec3595546f360671d3515b
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-04-27 07:24:54 +0530

    Update Module_pos.v

--- 03_module/Module_pos.v
+++ 03_module/Module_pos.v
@@ -1,1 +1,22 @@
+// ============================================
+// HDLBits Problem : Module
+// Topic          :  Module_pos
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-04-26
+// Link           : https://hdlbits.01xz.net/wiki/Module_pos
+// ============================================
 
+
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+
+    //instace by POSITION
+       mod_a inst1(out1,out2,a,b,c,d);
+    
+endmodule

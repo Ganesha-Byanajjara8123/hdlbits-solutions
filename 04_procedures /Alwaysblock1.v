d20e08e4548c632fe349ef10b8a4131c56df0100
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-05 21:41:13 +0530

    solve: Alwaysblock1-write AND gate with both assignment and always block

--- 04_procedures /Alwaysblock1.v
+++ 04_procedures /Alwaysblock1.v
@@ -1,1 +1,22 @@
 
+// ============================================
+// HDLBits Problem : Procedures 
+// Topic          :  Alwaysblock1
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-4
+// Link           : https://hdlbits.01xz.net/wiki/Alwaysblock1
+// ============================================
+
+
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    
+    assign out_assign = a & b;
+    always @(*) out_alwaysblock = a & b;
+
+endmodule

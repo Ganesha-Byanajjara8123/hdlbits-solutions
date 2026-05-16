35949f223844d6dc473dab84ad68d1492da88613
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-16 20:49:55 +0530

    solve: Reduction-using XOR gate

--- More_Verilog_Features/Reduction.v
+++ More_Verilog_Features/Reduction.v
@@ -1,1 +1,17 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic          :  Reduction
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-13
+// Link           : https://hdlbits.01xz.net/wiki/Reduction
+// ============================================
+
+
+module top_module (
+    input [7:0] in,
+    output parity); 
+    
+    assign parity = ^in[7:0];
+
+endmodule

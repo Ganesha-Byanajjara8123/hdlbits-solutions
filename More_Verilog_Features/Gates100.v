571b84268dcabc1bdd932aff1dd4640bce22a9ce
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-16 20:51:10 +0530

    solve: Gate100- using AND, OR, XOR

--- More_Verilog_Features/Gates100.v
+++ More_Verilog_Features/Gates100.v
@@ -1,1 +1,19 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  Gates100
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-14
+// Link           : https://hdlbits.01xz.net/wiki/Gates100
+// ============================================
+
+module top_module( 
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor 
+);
+    assign out_and = &in[99:0];
+    assign out_or = |in[99:0];
+    assign out_xor = ^in[99:0];
+endmodule

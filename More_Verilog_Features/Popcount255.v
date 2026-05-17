e27ad35796b9106ba0835e6da5d7db2c2a0accca
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-17 21:52:00 +0530

    solve: Popcount255-counting 1's

--- More_Verilog_Features/Popcount255.v
+++ More_Verilog_Features/Popcount255.v
@@ -1,1 +1,21 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  Popcount255
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-16
+// Link           : https://hdlbits.01xz.net/wiki/Popcount255
+// ============================================
+
+
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    always@(*) begin
+        out = 0;                 //declare default before assing value
+        for (int i=0; i<255; i++)
+            out = out + in[i];
+    end
+
+endmodule

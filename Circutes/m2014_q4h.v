21508bea8617ab25466179060065e62a00649038
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-21 20:13:55 +0530

    solve: m2014_q4h-out=in

--- Circutes/m2014_q4h.v
+++ Circutes/m2014_q4h.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  m2014_q4h
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-19
+// Link           : https://hdlbits.01xz.net/wiki/Exams/m2014_q4h
+// ============================================
+
+module top_module (
+    input in,
+    output out);
+
+    assign out = in;
+    
+endmodule
+
+

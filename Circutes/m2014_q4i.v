ba7d891cc8d696d11d97e1cd32d50fed127a8eff
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-24 19:01:51 +0530

    Update m2014_q4i.v

--- Circutes/m2014_q4i.v
+++ Circutes/m2014_q4i.v
@@ -1,1 +1,15 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  m2014_q4i
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-20
+// Link           : https://hdlbits.01xz.net/wiki/Exams/m2014_q4i
+// ============================================
+
+module top_module (
+    output out);
+
+    assign out = 1'b0;
+    
+endmodule

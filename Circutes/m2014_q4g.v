7956e057128a7179f130336d86e1b48fd40ad135
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-24 19:07:42 +0530

    solve: m2014_q4g-XNOR & XOR gates

--- Circutes/m2014_q4g.v
+++ Circutes/m2014_q4g.v
@@ -1,1 +1,30 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  m2014_q4g
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-23
+// Link           :https://hdlbits.01xz.net/wiki/Exams/m2014_q4g
+// ============================================
+
+
+
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+    
+    wire in0;
+    
+    //in behavioral
+    //assign in0 = ~(in1 ^ in2);
+    //assign out = (in3 ^ in0);
+    
+    //in structural
+    xnor (in0,in1,in2);
+    xor (out,in0,in3);
+    
+    
+
+endmodule

d2f31b2972819bce0bc8c53b23e4a6890a8bd3fd
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-24 19:05:35 +0530

    solve:   m2014_q4f-AND gate with one i/p inverse

--- Circutes/m2014_q4f.v
+++ Circutes/m2014_q4f.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  m2014_q4f
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-22
+// Link           : https://hdlbits.01xz.net/wiki/Exams/m2014_q4f
+// ============================================
+
+module top_module (
+    input in1,
+    input in2,
+    output out);
+
+    //and (out, in1,(~in2)); //in structural
+    assign out = in1 & (~in2); //in behavioral
+    
+endmodule

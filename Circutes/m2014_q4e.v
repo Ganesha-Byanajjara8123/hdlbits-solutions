e371d9d45ce6a1def958dc1dd80d839c0757543b
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-24 19:04:04 +0530

    solve: m2014_q4e-NOR gate

--- Circutes/m2014_q4e.v
+++ Circutes/m2014_q4e.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  More_verilog_features 
+// Topic           :  m2014_q4e
+// Solved by      : Byanajjara Ganesha
+// Date           : 2026-05-21
+// Link           : https://hdlbits.01xz.net/wiki/Exams/m2014_q4e
+// ============================================
+
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    
+    // assign out = ~(in1|in2); //in behavioral
+    nor (out,in1,in2); // in structural
+
+endmodule

0494f2a979b224aa926a0147033463215370d135
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-25 20:30:01 +0530

    solve: Gat

--- Circutes/Gate.v
+++ Circutes/Gate.v
@@ -1,6 +1,6 @@
 
 // ============================================
-// HDLBits Problem :  More_verilog_features 
+// HDLBits Problem :  Circuite 
 // Topic           :  Gates
 // Solved by      : Byanajjara Ganesha
 // Date           : 2026-05-24

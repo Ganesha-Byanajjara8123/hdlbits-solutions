99809ef42fb25de81272caf8d41897cfd9e6e2e4
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-25 20:27:48 +0530

    solve: m2014_q4g-XNOR & XOR gates

--- Circutes/m2014_q4g.v
+++ Circutes/m2014_q4g.v
@@ -1,6 +1,6 @@
 
 // ============================================
-// HDLBits Problem :  Circuite
+// HDLBits Problem :  Circuite 
 // Topic           :  m2014_q4g
 // Solved by      : Byanajjara Ganesha
 // Date           : 2026-05-23

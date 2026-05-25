d777609c7b7c1c471db450eb57cc3eb09bf9cb6e
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-25 20:27:15 +0530

    Update m2014_q4g.v

--- Circutes/m2014_q4g.v
+++ Circutes/m2014_q4g.v
@@ -1,6 +1,6 @@
 
 // ============================================
-// HDLBits Problem :  More_verilog_features 
+// HDLBits Problem :  Circuite
 // Topic           :  m2014_q4g
 // Solved by      : Byanajjara Ganesha
 // Date           : 2026-05-23

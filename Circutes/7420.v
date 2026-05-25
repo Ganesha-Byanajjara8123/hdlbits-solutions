6cd028eca03c3a1481939dac25a76c55e76789c7
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-25 20:26:44 +0530

    solve: 7420-chip with NAND gate

--- Circutes/7420.v
+++ Circutes/7420.v
@@ -1,1 +1,20 @@
 
+// ============================================
+// HDLBits Problem :  Circuite
+// Topic           :  7420
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-05-25
+// Link            :  https://hdlbits.01xz.net/wiki/7420
+// ============================================
+
+module top_module ( 
+    input p1a, p1b, p1c, p1d,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    
+    assign p1y = ~(p1a&p1b&p1c&p1d);
+    assign p2y = ~(p2a&p2b&p2c&p2d);
+
+
+endmodule

7761f25188f2b198cea1a3174aa9ad500e62ca1b
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-26 22:24:29 +0530

    solve: Truthtable-A/C Table and by using ternary operator & K-map

--- Circutes/Truthtable1.v
+++ Circutes/Truthtable1.v
@@ -1,1 +1,30 @@
 
+// ============================================
+// HDLBits Problem :  Circuite
+// Topic           :  Truthtable1
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-05-26
+// Link            :  https://hdlbits.01xz.net/wiki/Truthtable1
+// ============================================
+
+
+module top_module( 
+    input x3,
+    input x2,
+    input x1,  // three inputs
+    output f   // one output
+);
+    /* A/C truth table
+    assign f = (~x1 & x2 & ~x3) | 
+        (x1 & x2 & ~x3) |
+        (x1 & ~x2 & x3) |
+        (x1 & x2 & x3);
+        */
+    
+    //or we can also solve this by using ternary operator like x3 is the sel
+    //assign f = x3 ? x1 : x2;
+    
+    //or we have K-map solution like
+    assign f = (~x3 & x2) | (x3 & x1);
+    
+endmodule

eaeed450b0c98fd7f9bb1574be06b7333108b1e1
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-27 20:25:15 +0530

    Update 7420.v

--- Circutes/7420.v
+++ Circutes/7420.v
@@ -13,7 +13,7 @@ module top_module (
     input p2a, p2b, p2c, p2d,
     output p2y );
     
-    assign p1y = ~(p1a&p1b&p1c&p1d);
+    assign p1y = ~(p1a&p1b&p1c&p1d);  //written by given IC circuite
     assign p2y = ~(p2a&p2b&p2c&p2d);
 
 

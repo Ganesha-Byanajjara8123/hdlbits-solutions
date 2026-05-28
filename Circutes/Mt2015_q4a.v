3172be970073fd90aacdcb7a0e830324b1bc58e8
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-28 21:16:00 +0530

    Update Mt2015_q4a.v

--- Circutes/Mt2015_q4a.v
+++ Circutes/Mt2015_q4a.v
@@ -1,1 +1,14 @@
 
+// ============================================
+// HDLBits Problem :  Circuite
+// Topic           :  Mt2015_q4a
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-05-28
+// Link            : https://hdlbits.01xz.net/wiki/Mt2015_q4a
+// ============================================
+
+
+module top_module (input x, input y, output z);
+
+    assign z = (x^y) & x;
+endmodule

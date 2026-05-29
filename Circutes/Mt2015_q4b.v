1103ab3bffea6960e2436014a83a6ee6b6b7b375
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-05-29 21:34:30 +0530

    Update Mt2015_q4b.v

--- Circutes/Mt2015_q4b.v
+++ Circutes/Mt2015_q4b.v
@@ -1,1 +1,16 @@
 
+// ============================================
+// HDLBits Problem :  Circuite
+// Topic           :  Mt2015_q4b
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-05-29
+// Link            : https://hdlbits.01xz.net/wiki/Mt2015_q4b
+// ============================================
+
+
+module top_module ( input x, input y, output z );
+
+   //the given above wave form is output of the XNOR gate
+    assign z = ~(x ^ y); 
+    
+endmodule

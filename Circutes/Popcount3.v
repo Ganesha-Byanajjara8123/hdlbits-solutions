af21b44d86f0539630006acc9951423f72520827
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-02 09:40:28 +0530

    Update Popcount3.v

--- Circutes/Popcount3.v
+++ Circutes/Popcount3.v
@@ -1,1 +1,22 @@
 
+// ============================================
+// HDLBits Problem :  Circuite
+// Topic           :  Popcount3
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-02
+// Link            : https://hdlbits.01xz.net/wiki/Popcount3
+// ============================================
+
+
+
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    
+    always@(*) begin
+        out = 0;  //set default before counting no.of 1's
+        for(int i=0; i<3; i++) //make for loop with taking i as integer
+            out = out + in[i]; //here it counts number of 1's
+    end
+
+endmodule

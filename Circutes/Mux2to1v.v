99daaddaef6546055d10611dc0b10652d14f22fd
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-04 08:53:40 +0530

    Update Mux2to1v.v

--- Circutes/Mux2to1v.v
+++ Circutes/Mux2to1v.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-MUX's
+// Topic           :  Mux2to1v
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-06
+// Link            : https://hdlbits.01xz.net/wiki/Mux2to1v
+// ============================================
+
+
+module top_module( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+    
+    assign out = sel ? b : a;
+
+endmodule

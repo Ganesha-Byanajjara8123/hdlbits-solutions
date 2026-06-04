43b1914df1d54373058c38f9c1e7ddad7f16fd9a
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-04 08:55:31 +0530

    Update Mux9to1v.v

--- Circutes/Mux9to1v.v
+++ Circutes/Mux9to1v.v
@@ -1,1 +1,34 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-MUX's
+// Topic           :  Mux9to1v
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-07
+// Link            : https://hdlbits.01xz.net/wiki/Mux9to1v
+// ============================================
+
+
+module top_module( 
+    input [15:0] a, b, c, d, e, f, g, h, i,
+    input [3:0] sel,
+    output [15:0] out );
+    
+    always@(*) begin
+        case(sel)
+            4'd0:out=a;
+            4'd1:out=b;
+            4'd2:out=c;
+            4'd3:out=d;
+            4'd4:out=e;
+            4'd5:out=f;
+            4'd6:out=g;
+            4'd7:out=h;
+            4'd8:out=i;
+			
+            //instead of writting individual sel & out(9-15) just decalre them all in default value
+            default:out=16'hffff; 
+			
+        endcase
+    end
+
+endmodule

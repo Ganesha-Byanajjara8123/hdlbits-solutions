c1ace50d7cd36921e3362f368216cc8788d2e5b3
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-04 08:57:55 +0530

    Update Mux256to1.v

--- Circutes/Mux256to1.v
+++ Circutes/Mux256to1.v
@@ -1,1 +1,19 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-MUX's
+// Topic           :  Mux256to1
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-08
+// Link            : https://hdlbits.01xz.net/wiki/Mux256to1
+// ============================================
+
+
+module top_module( 
+    input [255:0] in,
+    input [7:0] sel,
+    output out );
+	
+    //sel=0 and in = 0, sel = 1 and in = 1...etc upto 256
+    assign out = in[sel];
+   
+endmodule

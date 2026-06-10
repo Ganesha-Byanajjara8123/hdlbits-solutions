5e1f03c42a8c9c525c6612e67969a2dd20809708
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-10 18:00:46 +0530

    solve: Hadd-a simple Half adder

--- Circutes/Hadd.v
+++ Circutes/Hadd.v
@@ -1,1 +1,19 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Arithmetic
+// Topic           :  Hadd
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-10
+// Link            :  https://hdlbits.01xz.net/wiki/Hadd
+// ============================================
+
+
+module top_module( 
+    input a, b,
+    output cout, sum );
+   
+    //we know the half adder equation, A/C that
+    assign sum = a^b;
+    assign cout = a&b | b&a;
+    
+endmodule

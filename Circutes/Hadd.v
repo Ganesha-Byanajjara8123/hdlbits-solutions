211a913cd8a6baa17a44ffc87e464ed1219c0367
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-10 18:01:12 +0530

    solve: Hadd-a simple Half adder

--- Circutes/Hadd.v
+++ Circutes/Hadd.v
@@ -6,8 +6,6 @@
 // Date            :  2026-06-10
 // Link            :  https://hdlbits.01xz.net/wiki/Hadd
 // ============================================
-
-
 module top_module( 
     input a, b,
     output cout, sum );

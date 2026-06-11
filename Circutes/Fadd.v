b31f1c4a9383dd2060e55117dec09c50fac2fb5f
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-11 07:09:19 +0530

    solve: Fadd.v-a simple Full adder

--- Circutes/Fadd.v
+++ Circutes/Fadd.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Arithmetic
+// Topic           :  Fadd
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-11
+// Link            :  https://hdlbits.01xz.net/wiki/Fadd
+// ============================================
+
+
+module top_module( 
+    input a, b, cin,
+    output cout, sum );
+
+    //A/c full adder equations
+    assign sum = a^b^cin;
+    assign cout = a&b | a&cin | b&cin;
+endmodule

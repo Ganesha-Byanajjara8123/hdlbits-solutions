90483c8214ebda215485f6cfe6527360ab953a34
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-13 08:25:30 +0530

    Update m2014_q4j.v

--- Circutes/m2014_q4j.v
+++ Circutes/m2014_q4j.v
@@ -1,1 +1,16 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Arithmetic
+// Topic           :  m2014_q4j
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-13
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/m2014_q4j
+// ============================================
+
+module top_module (
+    input [3:0] x,
+    input [3:0] y, 
+    output [4:0] sum);
+    
+  assign sum = x + y;
+endmodule

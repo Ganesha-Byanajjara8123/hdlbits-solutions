e62efa378fce44b8e1e2540fe776be894098ab5c
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-15 07:19:30 +0530

    Update Adder100.v

--- Circutes/Adder100.v
+++ Circutes/Adder100.v
@@ -1,1 +1,22 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Arithmetic
+// Topic           :  Adder100
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-15
+// Link            :  https://hdlbits.01xz.net/wiki/Adder100
+// ============================================
+
+
+module top_module (
+	input [99:0] a,
+	input [99:0] b,
+	input cin,
+	output cout,
+	output [99:0] sum
+);
+
+	// The concatenation {cout, sum} is a 101-bit vector.
+	assign {cout, sum} = a+b+cin;
+
+endmodule

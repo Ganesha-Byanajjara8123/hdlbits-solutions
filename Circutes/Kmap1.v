c2df423425ebe742118121b6e6653592c4125024
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-17 09:05:23 +0530

    Update Kmap1.v

--- Circutes/Kmap1.v
+++ Circutes/Kmap1.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  Kmap1
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-17
+// Link            :  https://hdlbits.01xz.net/wiki/Kmap1
+// ============================================
+
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    output out  ); 
+    //SOP & POS we can write both in OR gate
+    assign out = (a | b | c);
+endmodule

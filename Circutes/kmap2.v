6df203a131eb921ea949dc55e812b25463f736d2
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-18 08:36:01 +0530

    solve:kmap2.v with logic gates in both POS&SOP

--- Circutes/kmap2.v
+++ Circutes/kmap2.v
@@ -1,1 +1,25 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  Kmap2
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-18
+// Link            :  https://hdlbits.01xz.net/wiki/Kmap2
+// ============================================
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    
+   //for SOP     
+    assign out = ~c & ~b | ~d & ~a | c&d&b | a&~b&d; 
+    
+    //for POS
+   // assign out = (~a | ~b | c) & (c |~d | ~b) & (~c | d | ~a) &(~c | ~d | a | b);
+   
+    
+
+endmodule

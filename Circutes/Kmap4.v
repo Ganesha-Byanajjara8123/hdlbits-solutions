3068c452803087de20d16d5619a9fb585d7ef0cd
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-19 07:06:14 +0530

    solve: Kmap4.v by xor-gate

--- Circutes/Kmap4.v
+++ Circutes/Kmap4.v
@@ -1,1 +1,20 @@
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  Kmap4
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-19
+// Link            :  https://hdlbits.01xz.net/wiki/Kmap4
+// ============================================
+
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+
+    //wkt check board configuration
+    assign out = (a^b^c^d);
+endmodule
 

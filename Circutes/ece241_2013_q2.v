09efece10b791d7dcc5a3c841277d53af084448e
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-20 06:58:35 +0530

    solve: ece241_2013_q2.v both POS&SOP

--- Circutes/ece241_2013_q2.v
+++ Circutes/ece241_2013_q2.v
@@ -1,1 +1,21 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  ece241_2013_q2
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-20
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/ece241_2013_q2
+// ============================================
+
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output out_sop,
+    output out_pos
+); 
+    assign out_sop = c&d | ~a&~b&c&~d;
+    assign out_pos = (c | ~d) & (~b | d) & (~a | d) & (b | c);
+
+endmodule

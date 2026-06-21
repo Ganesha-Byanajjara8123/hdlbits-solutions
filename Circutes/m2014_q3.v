1e7857a307ca9aca65c199a3644280dbb0f3dc60
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-21 10:35:30 +0530

    solve:m2014_q3.v- by Vectors method

--- Circutes/m2014_q3.v
+++ Circutes/m2014_q3.v
@@ -1,1 +1,16 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  m2014_q3
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-21
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/m2014_q3
+// ============================================
+
+module top_module (
+    input [4:1] x, 
+    output f );
+    //by vectors method
+    assign f = (x[3]&~x[1]) | (x[1]&x[2]&x[4]);
+    
+endmodule

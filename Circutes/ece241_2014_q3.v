b60572de585adfad64aa63492461813a85f3181c
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-23 07:18:30 +0530

    solve: ece241_2014_q3.v with ternary operator

--- Circutes/ece241_2014_q3.v
+++ Circutes/ece241_2014_q3.v
@@ -1,1 +1,20 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-Kmaps
+// Topic           :  ece241_2014_q3
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-23
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/ece241_2014_q3
+// ============================================
+
+module top_module (
+    input c,
+    input d,
+    output [3:0] mux_in
+); 
+
+  	assign mux_in[0] = c ? 1 : d;          // 1 mux:   c|d
+	assign mux_in[1] = 0;                  // No muxes:  0
+	assign mux_in[2] = d ? 0 : 1;          // 1 mux:    ~d
+	assign mux_in[3] = c ? d : 0;          // 1 mux:   c&d
+endmodule

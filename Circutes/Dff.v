c5e8b199722f1acabe473a76b79a86683a877160
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-24 16:16:37 +0530

    solve: Dff.v - an simple D-FF

--- Circutes/Dff.v
+++ Circutes/Dff.v
@@ -1,1 +1,22 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Dff
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-24
+// Link            :  https://hdlbits.01xz.net/wiki/Dff
+// ============================================
+
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );//
+
+    // Use a clocked always block
+    //   copy d to q at every positive edge of clk
+    //   Clocked always blocks should use non-blocking assignments
+
+    always@(posedge clk) begin
+        q<=d;
+    end
+endmodule

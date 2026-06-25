61dca85ba002be77f217165333020bf7939e303f
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-25 19:51:00 +0530

    Update m2014_q4b.v

--- Circutes/m2014_q4b.v
+++ Circutes/m2014_q4b.v
@@ -1,1 +1,23 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  m2014_q4b
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-25
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/m2014_q4b
+// ============================================
+ 
+
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output q);
+
+    always@(posedge clk or posedge ar) begin
+        if(ar)
+            q<=1'b0;
+        else
+             q<=d;
+    end
+endmodule

eb397b0e57e021b976750d1d1f27e9461582ab70
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-26 17:24:17 +0530

    solve: Dff8r.v-with synchronous reset

--- Circutes/Dff8r.v
+++ Circutes/Dff8r.v
@@ -1,1 +1,22 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Dff8r
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-26
+// Link            :  https://hdlbits.01xz.net/wiki/Dff8r
+// ============================================
+
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always@(posedge clk) begin
+        if(reset) 
+            q<=1'b0;
+            else  
+                q<=d; 
+        end
+endmodule

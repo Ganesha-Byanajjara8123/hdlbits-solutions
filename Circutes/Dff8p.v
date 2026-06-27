f13eb59f30c4b89314454b1b0709cd5e82c845fe
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-27 07:59:13 +0530

    Update Dff8p.v

--- Circutes/Dff8p.v
+++ Circutes/Dff8p.v
@@ -1,1 +1,24 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Dff8p
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-27
+// Link            :  https://hdlbits.01xz.net/wiki/Dff8p
+// ============================================
+
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+    always@(negedge clk) begin
+       
+        if(reset) 
+            q<=8'h34; //The flip-flops must be reset to 0x34 rather than zero. 
+            else  
+                q<=d; 
+        end
+
+endmodule

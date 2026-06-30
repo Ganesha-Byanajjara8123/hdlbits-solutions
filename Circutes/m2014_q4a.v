b25d9e3113e3266e03515bc2e634cae2306482ed
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-06-30 10:58:11 +0530

    Update m2014_q4a.v

--- Circutes/m2014_q4a.v
+++ Circutes/m2014_q4a.v
@@ -1,1 +1,19 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  m2014_q4a
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-06-30
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/m2014_q4a
+// ============================================
+
+module top_module (
+    input d, 
+    input ena,
+    output q);
+
+    always@(*) begin
+        if(ena)
+            q<=d;
+    end
+endmodule

96042e6b4423eb870e5a3dacae443a6aad916e69
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-01 11:08:23 +0530

    Update m2014_q4d.v

--- Circutes/m2014_q4d.v
+++ Circutes/m2014_q4d.v
@@ -1,1 +1,18 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  m2014_q4d
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-01
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/m2014_q4d
+// ============================================
+
+module top_module (
+    input clk,
+    input in, 
+    output out);
+ 
+    always@(posedge  clk) begin
+        out<=in^out;
+    end
+endmodule

9e7364f01b116886b41829320f2e9b2f5cb63d0b
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-01 11:11:47 +0530

    solve: Mt2015_muxdff.v-by ternary operator

--- Circutes/Mt2015_muxdff.v
+++ Circutes/Mt2015_muxdff.v
@@ -1,1 +1,33 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Mt2015_muxdff
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-01
+// Link            :  https://hdlbits.01xz.net/wiki/Mt2015_muxdff
+// ============================================
+
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+
+    wire mux_out;
+    
+    assign mux_out = L ? r_in : q_in;
+    
+    always@(posedge clk) begin
+        Q<=mux_out;
+    end
+    
+   /* or we can also write it like
+   
+     always@(posedge clk) begin
+        Q<=L ? r_in : q_in;
+    end
+    
+    */
+   
+endmodule

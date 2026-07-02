119be3c65447524c6a542ace24d418159eac75b0
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-02 07:19:18 +0530

    Update ece241_2013_q7.v

--- Circutes/ece241_2013_q7.v
+++ Circutes/ece241_2013_q7.v
@@ -1,1 +1,20 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  ece241_2013_q7
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-02
+// Link            : https://hdlbits.01xz.net/wiki/Exams/ece241_2013_q7
+// ============================================
+
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+    //just apply JK FF Equation
+    always@(posedge clk) begin
+        Q<=j&(~Q)|(~k)&Q;
+    end
+
+endmodule

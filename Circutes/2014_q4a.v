99427800981f73936ebd2ef861441050a654bcdc
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-02 07:15:55 +0530

    Update 2014_q4a.v

--- Circutes/2014_q4a.v
+++ Circutes/2014_q4a.v
@@ -1,1 +1,20 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  2014_q4a
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-02
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/2014_q4a
+// ============================================
+
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+
+    always@(posedge clk) begin
+        Q <= L ? R : (E ? w : Q);
+    end
+         
+endmodule

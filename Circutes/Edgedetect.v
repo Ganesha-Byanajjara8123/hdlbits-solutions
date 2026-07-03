73847e6d150eae193c8bf4169d65b557fb3a7710
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-03 08:41:01 +0530

    solve:   Edgedetect.v-current & last cycle values

--- Circutes/Edgedetect.v
+++ Circutes/Edgedetect.v
@@ -1,1 +1,23 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Edgedetect
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-03
+// Link            : https://hdlbits.01xz.net/wiki/Edgedetect
+// ============================================
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0]in_last_cycle;
+    
+    always@(posedge clk) begin
+        in_last_cycle <= in;// for last state cycle values
+        pedge <= in & (~in_last_cycle); //for current state values
+        
+    end
+
+endmodule

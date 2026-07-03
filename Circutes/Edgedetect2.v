004c532d5fc55ffe1054f99a26e87277bbd79b57
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-03 08:42:28 +0530

    solev: Edgedetect2.v-current&previous state i/ps

--- Circutes/Edgedetect2.v
+++ Circutes/Edgedetect2.v
@@ -1,1 +1,24 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-D-FF
+// Topic           :  Edgedetect2
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-07-03
+// Link            : https://hdlbits.01xz.net/wiki/Edgedetect2
+// ============================================
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg[7:0] last_in_cycle;
+    
+    always@(posedge clk) begin
+        last_in_cycle <= in; //to store previous state values
+		 
+		// the bit wise operator (^) will check 2-binary signals are diff bit by bit
+		anyedge <= in ^ last_in_cycle;
+    end
+
+endmodule

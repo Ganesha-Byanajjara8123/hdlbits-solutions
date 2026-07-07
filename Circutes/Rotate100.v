66d64b992741ae93833585f12a2bc1b17d0d69e0
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-07 09:04:10 +0530

    solve: Rotate100.v roatate left&right

--- Circutes/Rotate100.v
+++ Circutes/Rotate100.v
@@ -6,7 +6,7 @@
 // Date            :  2026-07-07
 // Link            :  https://hdlbits.01xz.net/wiki/Rotate100
 // ============================================
-
+ 
 module top_module(
     input clk,
     input load,

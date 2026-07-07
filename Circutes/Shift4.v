8345e2f066ed26d97f95ba7c8231d45b912b9e57
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-07 09:02:48 +0530

    solve: Shift4.v shift enable

--- Circutes/Shift4.v
+++ Circutes/Shift4.v
@@ -6,7 +6,7 @@
 // Date            :  2026-07-07
 // Link            :  https://hdlbits.01xz.net/wiki/Shift4
 // ============================================
-
+ 
 module top_module(
     input clk,
     input areset,  // async active-high reset to zero

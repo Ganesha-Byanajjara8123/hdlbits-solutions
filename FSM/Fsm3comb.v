45b5fdb683fdb5dd76acc9fede35ac291c350c07
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-14 07:38:35 +0530

    Create Fsm3comb.v

---
+++ FSM/Fsm3comb.v
@@ -0,0 +1,30 @@
+// ============================================
+// HDLBits Problem :  Circuite-FSM
+// Topic           :  Fsm3comb
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-14-07
+// Link            :  https://hdlbits.01xz.net/wiki/Fsm3comb
+// ============================================
+
+module top_module(
+    input in,
+    input [1:0] state,
+    output [1:0] next_state,
+    output out); //
+
+    parameter 	
+
+    //State transition logic: next_state = f(state, in)
+    always@(*)begin
+        case(state)
+            A : next_state = in ? B : A;
+            B : next_state = in ? B : C;
+            C : next_state = in ? D : A;
+            D : next_state = in ? B : C;
+        endcase 
+    end
+
+    // Output logic:  out = f(state) for a Moore state machine
+    assign out = (state == D);
+
+endmodule

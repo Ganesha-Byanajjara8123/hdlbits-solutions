49dd0a11034e75104db789a6fac0021c2f8d0750
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-15 07:05:10 +0530

    Update Fsm3.v

--- FSM/Fsm3.v
+++ FSM/Fsm3.v
@@ -1,1 +1,42 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-FSM
+// Topic           :  Fsm3
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-15-07
+// Link            :  https://hdlbits.01xz.net/wiki/Fsm3
+// ============================================
+
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+    
+    parameter A=0, B=1, C=2, D=3;
+    reg [1:0] state, next;
+
+    // State transition logic
+    always@(*)begin
+        case(state)
+            A : next = in ? B : A;
+            B : next = in ? B : C;
+            C : next = in ? D : A;
+            D : next = in ? B : C;
+        endcase 
+    end
+
+
+
+    // State flip-flops with asynchronous reset
+    always@(posedge clk or posedge areset)begin
+        if(areset)
+            state <= A;
+        else
+            state <= next;
+    end
+
+    // Output logic
+    assign out = (state == D);
+
+endmodule

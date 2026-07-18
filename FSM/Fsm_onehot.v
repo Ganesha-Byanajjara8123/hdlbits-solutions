d97838cbd27a2e0fc16f2ce7b432dbc4d9d40095
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-18 10:18:21 +0530

    solve: Fsm_onehot.v

--- FSM/Fsm_onehot.v
+++ FSM/Fsm_onehot.v
@@ -1,1 +1,36 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-FSM
+// Topic           :  Fsm_onehot
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-18-07
+// Link            :  https://hdlbits.01xz.net/wiki/Fsm_onehot
+// ============================================
+
+module top_module (
+    input in,
+    input [9:0] state,
+    output [9:0] next_state,
+    output out1,
+    output out2
+);
+
+    // State transition logic derived by inspection
+assign next_state[0] = (state[0] & ~in) | (state[1] & ~in) | (state[2] & ~in) | 
+                           (state[3] & ~in) | (state[4] & ~in) | (state[7] & ~in) | 
+                           (state[8] & ~in) | (state[9] & ~in);    
+    assign next_state[1] = in & (state[0] | state[8] | state[9]);
+    assign next_state[2] = in & state[1];
+    assign next_state[3] = in & state[2];
+    assign next_state[4] = in & state[3];
+    assign next_state[5] = in & state[4];
+    assign next_state[6] = in & state[5];
+    assign next_state[7] = in & (state[6] | state[7]);
+    assign next_state[8] = ~in & state[5];
+    assign next_state[9] = ~in & state[6];
+
+    // Output logic
+assign out1 = state[8] | state[9];
+    assign out2 = state[7] | state[9];
+
+endmodule

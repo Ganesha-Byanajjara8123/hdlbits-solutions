4fac03994192d6b3093a1301346277e5678f7d0d
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-19 16:19:24 +0530

    solve: IC_7458.v

--- 01_basics/IC_7458.v
+++ 01_basics/IC_7458.v
@@ -6,7 +6,6 @@
 // Link           : https://hdlbits.01xz.net/wiki/7458
 // ============================================
 
-
 module top_module ( 
     input p1a, p1b, p1c, p1d, p1e, p1f,
     output p1y,

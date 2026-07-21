1ef0c5227dc05e089093b01704fffb37f7b3c9ad
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-21 07:50:40 +0530

    solve: Fsm_serialdp.v

--- FSM/Fsm_serialdp.v
+++ FSM/Fsm_serialdp.v
@@ -6,7 +6,7 @@
 // Date            :  2026-21-07
 // Link            :  https://hdlbits.01xz.net/wiki/Fsm_serialdp
 // ============================================
-
+ 
 module top_module (
     input clk,
     input in,

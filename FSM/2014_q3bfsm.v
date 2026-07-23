d7503a5cc4d09c6c4f6a9703e9eda58922e095d8
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-23 19:16:40 +0530

    Update 2014_q3bfsm.v

--- FSM/2014_q3bfsm.v
+++ FSM/2014_q3bfsm.v
@@ -1,1 +1,40 @@
 
+// ============================================
+// HDLBits Problem :  Circuite-FSM
+// Topic           :  2014_q3bfsm
+// Solved by       :  Byanajjara Ganesha
+// Date            :  2026-23-07
+// Link            :  https://hdlbits.01xz.net/wiki/Exams/2014_q3bfsm
+// ============================================
+
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input x,
+    output z
+);
+    parameter A = 3'b000, B = 3'b001, C = 3'b010, D = 3'b011, E = 3'b100;
+    reg [2:0] state, next;
+    
+    always@(posedge clk)begin
+        if(reset)
+            state <= A;
+        else 
+            state <= next;
+    end
+    
+    always@(*)begin
+        case(state)
+            A : next = x ? B : A;
+            B : next = x ? E : B;
+            C : next = x ? B : C;
+            D : next = x ? C : B;
+            E : next = x ? E : D;
+            default : next = A;
+        endcase
+    end
+    
+    assign z = (state == D || state == E);
+            
+
+endmodule
